f8e060f6556987ae2a4f8be6fbb0db8b5e772376
Geundol222 <geundol222@gmail.com>
2023-05-24 21:48:02 +0900

    feat : HomeWork Start

---
+++ Assets/HomeWork/2023.05.24/Scripts/StateMachine/StateBase0524.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateBase0524<TOwner> where TOwner : MonoBehaviour
+{
+    public TOwner owner;
+
+    public StateBase0524(TOwner owner)
+    {
+        this.owner = owner;
+    }
+
+    public abstract void Setup();
+
+    public abstract void Enter();
+
+    public abstract void Update();
+
+    public abstract void Exit();
+}
